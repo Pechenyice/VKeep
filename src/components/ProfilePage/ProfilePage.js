7a134b8a742c1ebed8d35d998cd6b90c35e73d68
Pechenyice <german.iskhakov@mail.ru>
2021-08-27 00:45:47 +0300

    added statistic and sessions layout

--- src/components/ProfilePage/ProfilePage.js
+++ src/components/ProfilePage/ProfilePage.js
@@ -6,6 +6,8 @@ import ProfileInfo from "../ProfileInfo/ProfileInfo";
 import LayoutManager from "../LayoutManager/LayoutManager";
 import ProfileActivity from "../ProfileActivity/ProfileActivity";
 import ProfileStatInfo from "../ProfileStatInfo/ProfileStatInfo";
+import ProfileStatistic from "../ProfileStatistic/ProfileStatistic";
+import ProfileSessions from "../ProfileSessions/ProfileSessions";
 
 function ProfilePage() {
 
@@ -62,11 +64,22 @@ function ProfilePage() {
 
     let [size, setSize] = useState(false);
 
+    let [platformSelected, setPlatformSelected] = useState('');
+
     function handleActivityResize() {
+
         setSize(!size);
 
-        
     }
+
+    function handleSessionHovered(platform) {
+        setPlatformSelected(platform);
+    }
+
+    function handleSessionLeaved() {
+        setPlatformSelected('');
+    }
+
 
     return (
         <section className={styles.profilePage}>
@@ -90,6 +103,15 @@ function ProfilePage() {
                 <LayoutManager stylesheet={{maxHeight: '570px'}} columns={1}>
                     <ProfileStatInfo />
                 </LayoutManager>
+
+                <LayoutManager stylesheet={{maxHeight: '570px'}} columns={1}>
+                    <ProfileStatistic platformSelected={platformSelected}/>
+                </LayoutManager>
+
+                <LayoutManager columns={2}>
+                    <ProfileSessions onHover={handleSessionHovered} onLeave={handleSessionLeaved} />
+                </LayoutManager>
+
             </div>
 
         </section>
