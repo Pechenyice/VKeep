2af06b5491f0fe7691e20c214feab3bf624acff6
Pechenyice <german.iskhakov@mail.ru>
2021-08-26 17:42:43 +0300

    added new svg to manager

--- src/svgs/svgManager.js
+++ src/svgs/svgManager.js
@@ -13,6 +13,34 @@ let svgs = {
             <path fill-rule="evenodd" clip-rule="evenodd" d="M19.7071 11.2929C20.0976 11.6834 20.0976 12.3166 19.7071 12.7071L12.7071 19.7071C12.3166 20.0976 11.6834 20.0976 11.2929 19.7071C10.9024 19.3166 10.9024 18.6834 11.2929 18.2929L17.5858 12L11.2929 5.70711C10.9024 5.31658 10.9024 4.68342 11.2929 4.29289C11.6834 3.90237 12.3166 3.90237 12.7071 4.29289L19.7071 11.2929Z" fill="white"/>
         </svg>,
     
+    moreActivityContent:
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M6 9L12 15L18 9" stroke="#2787F5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>,
+
+    'activity-name-svg':
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M16 21V19C16 17.9391 15.5786 16.9217 14.8284 16.1716C14.0783 15.4214 13.0609 15 12 15H5C3.93913 15 2.92172 15.4214 2.17157 16.1716C1.42143 16.9217 1 17.9391 1 19V21" stroke="#2787F5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M17 11L19 13L23 9" stroke="#2787F5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M8.5 11C10.7091 11 12.5 9.20914 12.5 7C12.5 4.79086 10.7091 3 8.5 3C6.29086 3 4.5 4.79086 4.5 7C4.5 9.20914 6.29086 11 8.5 11Z" stroke="#2787F5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>,
+
+    'activity-status-svg':
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M16 21V19C16 17.9391 15.5786 16.9217 14.8284 16.1716C14.0783 15.4214 13.0609 15 12 15H5C3.93913 15 2.92172 15.4214 2.17157 16.1716C1.42143 16.9217 1 17.9391 1 19V21" stroke="#2787F5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M17 11L19 13L23 9" stroke="#2787F5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M8.5 11C10.7091 11 12.5 9.20914 12.5 7C12.5 4.79086 10.7091 3 8.5 3C6.29086 3 4.5 4.79086 4.5 7C4.5 9.20914 6.29086 11 8.5 11Z" stroke="#2787F5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>,
+
+    'activity-image-svg':
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M19 3H5C3.89543 3 3 3.89543 3 5V19C3 20.1046 3.89543 21 5 21H19C20.1046 21 21 20.1046 21 19V5C21 3.89543 20.1046 3 19 3Z" stroke="#2787F5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M21 15L16 10L5 21" stroke="#2787F5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M8.5 10C9.32843 10 10 9.32843 10 8.5C10 7.67157 9.32843 7 8.5 7C7.67157 7 7 7.67157 7 8.5C7 9.32843 7.67157 10 8.5 10Z" stroke="#2787F5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+    
+    
+
     
 
 };
