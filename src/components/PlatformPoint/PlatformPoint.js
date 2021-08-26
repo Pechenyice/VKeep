476ff81aa5190050f7fed0d4c41e98c2df339a3a
Pechenyice <german.iskhakov@mail.ru>
2021-08-26 15:40:49 +0300

    static point for platforms

---
+++ src/components/PlatformPoint/PlatformPoint.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from './PlatformPoint.module.css';
+import PropTypes from 'prop-types';
+import Colors from "../../variables/Colors";
+
+function PlatformPoint({platform}) {
+
+    return (
+        <div className={styles.profileStatusWrapper} style={{ backgroundColor: Colors[platform] }}>
+
+        </div>
+    );
+
+}
+
+PlatformPoint.propTypes = {
+    platform: PropTypes.string
+}
+
+export default PlatformPoint;
