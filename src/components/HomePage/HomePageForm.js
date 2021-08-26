fc916e029ec95b908018af9a90060d32ff7feeeb
Pechenyice <german.iskhakov@mail.ru>
2021-08-26 03:29:59 +0300

    creating logic for input form and fix label for attribute

--- src/components/HomePage/HomePageForm.js
+++ src/components/HomePage/HomePageForm.js
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from './HomePage.module.css';
 import svgManager from "../../svgs/svgManager";
 
 function HomePageForm() {
+
+    let [vkUser, setVkUser] = useState('');
+
+    function validateInput(input) {
+        console.log('I don\'t care your input, send me a porn video if u want')
+    }
+
+    function handleVkUserInput(e) {
+
+        validateInput(e.target.value);
+
+        setVkUser(e.target.value);
+    }
+
+    function handleVkUserSubmit() {
+        setVkUser('');
+    }
     
     return (
         <div className={styles.formWrapper}>
-            <label for='vkUser' className={styles.inputLabel}>Добавить пользователя</label>
+            <label htmlFor='vkUser' className={styles.inputLabel}>Добавить пользователя</label>
             <div className={styles.formContainer}>
                 <div className={styles.inputWrapper}>
-                    <input className={styles.input} type='text' name='vkUser' placeholder='ID или domain' />
+                    <input className={styles.input} type='text' name='vkUser' placeholder='ID или domain' value={vkUser} onInput={handleVkUserInput}/>
                     <div className={styles.svgWapper}>{svgManager.getSvg('userProfile')}</div>
                 </div>
-                <button className={styles.inputSubmit} >
+                <button className={styles.inputSubmit} onClick={handleVkUserSubmit}>
                     {svgManager.getSvg('buttonSubmit')}
                 </button>
             </div>
