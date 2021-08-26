1ae0c6087d5ac69e4fc3f3f855c533ce1f598943
Pechenyice <german.iskhakov@mail.ru>
2021-08-27 00:43:58 +0300

    added todo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import './App.css';
 import HomePage from './components/HomePage/HomePage';
 import ProfilePage from './components/ProfilePage/ProfilePage';
 
+// TODO:
+// 1. New profile image in profile activity
+// 2. Creating and filling API manager
+
 function App() {
   return (
 
