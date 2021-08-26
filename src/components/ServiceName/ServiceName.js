0f7543b45ed96eb0790e524b9c06e55e58298083
Pechenyice <german.iskhakov@mail.ru>
2021-08-26 17:42:28 +0300

    added layout and style for new service name template

---
+++ src/components/ServiceName/ServiceName.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from './ServiceName.module.css';
+import PropTypes from 'prop-types';
+
+function ServiceName({name, displayCount, count}) {
+
+    return (
+        <div className={styles.serviceNameWrapper}>
+            <h2 className={styles.serviceName}>{name}</h2>
+            {displayCount ? <div className={styles.serviceCount}>{count}</div> : null}
+        </div>
+    );
+
+}
+
+ServiceName.propTypes = {
+    name: PropTypes.string,
+    displayCount: PropTypes.bool,
+    count: PropTypes.number
+}
+
+export default ServiceName;
