2380969c958cc2f9901243576ddfc38f4b0b049e
Pechenyice <german.iskhakov@mail.ru>
2021-08-28 04:30:42 +0300

    created more comfortable activity managment component

---
+++ src/components/ProfileActivity/ProfileActivityManager.js
@@ -0,0 +1,51 @@
+import React from "react";
+import PropTypes from "prop-types";
+import ProfileActivityNote from "./ProfileActivityNote";
+
+function ProfileActivityManager({content}) {
+
+    function constructUpdates() {
+        let names = content.names;
+        names = names.map(n => Object.assign({}, n, {'type': 'name'}));
+
+        let statuses = content.statuses;
+        statuses = statuses.map(n => Object.assign({}, n, {'type': 'status'}));
+
+        let avatars = content.avatars;
+        avatars = avatars.map(n => Object.assign({}, n, {'type': 'avatar'}));
+
+        let notes = [];
+
+        let all = names.concat(statuses).concat(avatars);
+
+        all.sort((a, b) => b - a);
+
+        for (let n in all) {
+
+            if (all[n].isInitial) continue;
+
+            notes.push(
+                <ProfileActivityNote key={n} content={all[n]} />
+            );
+        }
+
+        return notes;
+    }
+
+    return (
+
+        <div>
+            {
+                constructUpdates()
+            }
+        </div>
+
+    );
+
+}
+
+ProfileActivityManager.propTypes = {
+    content: PropTypes.object
+}
+
+export default ProfileActivityManager;
