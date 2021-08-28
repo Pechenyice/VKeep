317645332b4d5a69d7b3489353cbfa61fb66ae73
Pechenyice <german.iskhakov@mail.ru>
2021-08-29 02:41:30 +0300

    updated todo

--- src/App.js
+++ src/App.js
@@ -4,10 +4,9 @@ import ProfilePage from './components/ProfilePage/ProfilePage';
 
 // TODO:
 // 1. New profile image in profile activity and profile info
-// 2. Add skelet loading
-// 3. Adaptive
-// 4. Polishing layouts
-// 5. Sessions and stats revitalization
+// 2. Adaptive
+// 3. transition to resize and remove paddingRight from activity block
+// 4. Refactor Promise.all in api requests to Promise.then
 
 function App() {
   return (
