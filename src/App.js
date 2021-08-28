09f877e2f928fd90c120df5c36404dba8faa0f16
Pechenyice <german.iskhakov@mail.ru>
2021-08-28 04:25:20 +0300

    added todos

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import HomePage from './components/HomePage/HomePage';
 import ProfilePage from './components/ProfilePage/ProfilePage';
 
 // TODO:
-// 1. New profile image in profile activity
-// 2. Creating and filling API manager
+// 1. New profile image in profile activity and profile info
+// 2. Add skelet loading
+// 3. Adaptive
+// 4. Polishing layouts
+// 5. Sessions and stats revitalization
 
 function App() {
   return (
 
     // <HomePage /> 
 
-    <ProfilePage />
+    <ProfilePage user={'1'}/>
 
   );
 }
