7d64c7e0a4297a0d55d1028a2d23c37ab89a9be4
Pechenyice <german.iskhakov@mail.ru>
2021-08-29 02:44:55 +0300

    refactor for scrolling

--- src/components/ProfileActivity/ProfileActivity.js
+++ src/components/ProfileActivity/ProfileActivity.js
@@ -4,30 +4,53 @@ import PropTypes from 'prop-types';
 import ServiceName from "../ServiceName/ServiceName";
 import svgManager from './../../svgs/svgManager';
 import ProfileActivityManager from "./ProfileActivityManager";
+import Skeleton from "../Skeleton/Skeleton";
 
 function ProfileActivity({content, onResize, size}) {
 
-    
+    function countRenderedUpdates() {
+        let count = 0;
+        for (let s of content.statuses) {
+            count += s.isInitial ? 1 : 0;
+        }
+
+        for (let a of content.avatars) {
+            count += a.isInitial ? 1 : 0;
+        }
+
+        for (let n of content.names) {
+            count += n.isInitial ? 1 : 0;
+        }
+
+        return count;
+    }
 
     return (
-        <div className={styles.wrapperContainer} style={size ? {height: '1020px'} : {}}>
-            <div className={styles.wrapper}>
-                <ServiceName name={'Активность'} displayCount={true} count={
-                    Object.keys(content).length ? content.statuses.length + content.avatars.length + content.names.length : 0
-                    } />
+        // <div className={styles.wrapperContainerBox} style={size ? {height: '1020px'} : {}}>
+            <div className={styles.wrapperContainer} style={size ? {height: '1020px'} : {}}>
+                <div className={styles.wrapper}>
+                    <ServiceName name={'Активность'} displayCount={true} count={
+                        Object.keys(content).length ? countRenderedUpdates() : '-'
+                        } />
 
-                <div className={styles.activities} style={size ? {height: '900px'} : {}}>
-                    {
-                        Object.keys(content).length ? <ProfileActivityManager content={content}/> : null
-                    }
-                </div>
+                    <div className={styles.activities} style={size ? {height: '900px', overflowY: 'auto'} : {}}>
+                        {
+                            Object.keys(content).length ? 
+                            <ProfileActivityManager content={content}/> : 
+                            <div>
+                                <Skeleton type={'text'} animatorBackground={'var(--white)'} stylesheet={{width: '100%', marginBottom: '30px'}} />
+                                <Skeleton type={'text'} animatorBackground={'var(--white)'} stylesheet={{width: '100%'}} />
+                            </div>
+                        }
+                    </div>
 
-                <div className={styles.more} onClick={onResize}>
-                    <p className={styles.moreText}>{size ? 'Скрыть' :'Показать всё'}</p>
-                    <div className={styles.moreIcon} style={size ? {transform: 'rotateZ(180deg)', marginTop: '-8px'} : {}}>{svgManager.getSvg('moreActivityContent')}</div>
+                    <div className={styles.more} onClick={onResize}>
+                        <p className={styles.moreText}>{size ? 'Скрыть' :'Показать всё'}</p>
+                        <div className={styles.moreIcon} style={size ? {transform: 'rotateZ(180deg)', marginTop: '-8px'} : {}}>{svgManager.getSvg('moreActivityContent')}</div>
+                    </div>
                 </div>
             </div>
-        </div>
+        // </div>
     );
 
 }
