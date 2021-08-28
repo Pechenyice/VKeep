daf57239f658f4c0b521302f4ed26cd03417237f
Pechenyice <german.iskhakov@mail.ru>
2021-08-28 04:25:52 +0300

    created api manager

--- src/api/API.js
+++ src/api/API.js
@@ -1,2 +1,44 @@
 
-// export {getPCMinutes, getAndroidMinutes etc...}
+let HOST = 'http://localhost';
+let PORT = '3001';
+let API_URL = '/api';
+
+function constructUrl(endpoint, u) {
+    let isId = Number(u) != NaN;
+
+    return `${API_URL}${endpoint}?${isId ? 'id' : 'user'}=${u}`;
+}
+
+let API = {
+    getUser: async (u) => {
+
+        let res = await fetch(constructUrl('/users', u));
+
+        res = await res.json();
+
+        return res;
+
+    },
+
+    getSessions: async (u) => {
+
+        let res = await fetch(constructUrl('/sessions', u));
+
+        res = await res.json();
+
+        return res;
+
+    },
+
+    getUpdates: async (u) => {
+
+        let res = await fetch(constructUrl('/updates', u));
+
+        res = await res.json();
+
+        return res;
+
+    }
+}
+
+export default API;
