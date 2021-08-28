9b47c20c6957942ebebca7a2d910174aec39f78f
Pechenyice <german.iskhakov@mail.ru>
2021-08-29 02:44:25 +0300

    staarted refactor to transition animation

--- src/components/LayoutManager/LayoutManager.js
+++ src/components/LayoutManager/LayoutManager.js
@@ -2,21 +2,44 @@ import React from "react";
 import styles from './LayoutManager.module.css';
 import PropTypes from 'prop-types';
 
-function LayoutManager({columns, stylesheet = {}, children}) {
+function LayoutManager({columns, stylesheet = {}, size=false, resizing=false, children}) {
 
     let classes = [styles.flexColumns, styles[`flexColumns-${columns}`]];
 
-    return (
-        <section className={classes.join(' ')} style={stylesheet}>
-            {children}
-        </section>
-    );
+    function selectWrapper() {
+
+        if (size) {
+            return (
+
+                <div className={classes.join(' ')} style={{padding: '0px'}} >
+                    <section className={classes.join(' ')} style={stylesheet}>
+                        {children}
+                    </section>
+                </div>
+
+            );
+        } else {
+            return (
+                // <div style={resizing ? {padding: '0px', position: 'absolute'} : {}}>
+                    <section className={classes.join(' ')} style={stylesheet}>
+                        {children}
+                    </section>
+                // </div>
+
+            );
+        }
+
+    }
+
+    return selectWrapper();
 
 }
 
 LayoutManager.propTypes = {
     columns: PropTypes.number,
     stylesheet: PropTypes.object,
+    size: PropTypes.bool,
+    resizing: PropTypes.bool,
     children: PropTypes.element
 }
 
