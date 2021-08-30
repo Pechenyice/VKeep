0fcae78de5c123859670ab5f17a03d34098b7192
Pechenyice <german.iskhakov@mail.ru>
2021-08-30 03:40:59 +0300

    added interlayer component for resing activities

---
+++ src/components/ProfileActivity/ProfileActivityResizer.js
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from './ProfileActivity.module.css';
+import ProfileActivity from "./ProfileActivity";
+
+function ProfileActivityResizer({onResize, content, size}) {
+
+    let classes = [styles.profileActivityResizeWrapper];
+
+    if (size) classes.push(styles.profileActivityResizeWrapperMaxed);
+    
+    return (
+        <div className={styles.profileActivityNormalisedSize}>
+            <div className={classes.join(' ')}>
+                <ProfileActivity onResize={onResize} content={content} size={size}/>
+            </div>
+        </div>
+    );
+
+}
+
+ProfileActivityResizer.propTypes = {
+    content: PropTypes.object,
+    onResize: PropTypes.func,
+    size: PropTypes.bool
+}
+
+export default ProfileActivityResizer;
