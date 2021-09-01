8682c90c4f541a45fc0d50f70e255f1348817d33
Pechenyice <german.iskhakov@mail.ru>
2021-09-01 15:27:18 +0300

    deleted TODO

--- src/App.js
+++ src/App.js
@@ -2,12 +2,6 @@ import './App.css';
 import HomePage from './components/HomePage/HomePage';
 import ProfilePage from './components/ProfilePage/ProfilePage';
 
-// TODO:
-// 1. New profile image in profile activity and profile info
-// 2. Adaptive
-// 3. transition to resize and remove paddingRight from activity block
-// 4. Refactor Promise.all in api requests to Promise.then
-
 function App() {
   return (
 
