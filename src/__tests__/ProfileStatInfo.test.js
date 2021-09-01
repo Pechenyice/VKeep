967736bb0e6a2370ac71815875a3a71254576b17
Pechenyice <german.iskhakov@mail.ru>
2021-09-01 15:24:33 +0300

    API powered state managment tests

---
+++ src/__tests__/ProfileStatInfo.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { shallow, configure } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import ProfileStatInfo from '../components/ProfileStatInfo/ProfileStatInfo';
+
+
+configure({ adapter: new Adapter() });
+
+describe('Shallow tests: Profile aside stats', () => {
+    let wrapper;
+
+    describe('some API content is not ready', () => {
+        beforeEach(() => {
+            wrapper = shallow(<ProfileStatInfo content={{'userInfo': {}, 'sessions': [], 'updates': {}}} />);
+        });
+    
+        it('should render skeletons', () => {
+            expect(
+                wrapper.find('StatInfoSpec').first().props().fillSkeleton
+            ).toBe(true);
+        });
+    });
+
+    describe('all API content is not ready', () => {
+        beforeEach(() => {
+            wrapper = shallow(<ProfileStatInfo content={{'userInfo': {'test': true}, 'sessions': ['test'], 'updates': {'statuses': [], 'names': [], 'avatars': []}}} />);
+        });
+    
+        it('should render skeletons', () => {
+            expect(
+                wrapper.find('StatInfoSpec').first().props().fillSkeleton
+            ).toBe(false);
+        });
+    });
+});
