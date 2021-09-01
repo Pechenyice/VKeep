861b92877bdb7bd87487092093c9f32082030e76
Pechenyice <german.iskhakov@mail.ru>
2021-09-01 15:28:12 +0300

    styling

--- src/components/ProfileActivity/ProfileActivity.js
+++ src/components/ProfileActivity/ProfileActivity.js
@@ -6,8 +6,7 @@ import svgManager from './../../svgs/svgManager';
 import ProfileActivityManager from "./ProfileActivityManager";
 import Skeleton from "../Skeleton/Skeleton";
 
-function ProfileActivity({content, onResize, size}) {
-
+function ProfileActivity({ content, onResize, size }) {
     function countRenderedUpdates() {
         let count = 0;
         for (let s of content.statuses) {
@@ -30,33 +29,28 @@ function ProfileActivity({content, onResize, size}) {
     if (size) classes.push(styles.maxedActivities);
 
     return (
-        // <div className={styles.wrapperContainerBox} style={size ? {height: '1020px'} : {}}>
-            // <div className={styles.wrapperContainer} style={size ? {height: '1020px'} : {}}>
-                <div className={styles.wrapper}>
-                    <ServiceName name={'Активность'} displayCount={true} count={
-                        Object.keys(content).length ? countRenderedUpdates() : '-'
-                        } />
+        <div className={styles.wrapper}>
+            <ServiceName name={'Активность'} displayCount={true} count={
+                Object.keys(content).length ? countRenderedUpdates() : '-'
+            } />
 
-                    <div className={classes.join(' ')}>
-                        {
-                            Object.keys(content).length ? 
-                            <ProfileActivityManager content={content}/> : 
-                            <div>
-                                <Skeleton type={'text'} animatorBackground={'var(--white)'} stylesheet={{width: '100%', marginBottom: '30px'}} />
-                                <Skeleton type={'text'} animatorBackground={'var(--white)'} stylesheet={{width: '100%'}} />
-                            </div>
-                        }
-                    </div>
+            <div className={classes.join(' ')}>
+                {
+                    Object.keys(content).length ?
+                        <ProfileActivityManager content={content} /> :
+                        <div className={styles.skeletonsWrapper}>
+                            <Skeleton type={'text'} animatorBackground={'var(--white)'} stylesheet={{ width: '100%', marginBottom: '30px' }} />
+                            <Skeleton type={'text'} animatorBackground={'var(--white)'} stylesheet={{ width: '100%' }} />
+                        </div>
+                }
+            </div>
 
-                    <div className={styles.more} onClick={onResize}>
-                        <p className={styles.moreText}>{size ? 'Скрыть' :'Показать всё'}</p>
-                        <div className={styles.moreIcon} style={size ? {transform: 'rotateZ(180deg)', marginTop: '-8px'} : {}}>{svgManager.getSvg('moreActivityContent')}</div>
-                    </div>
-                </div>
-            // </div>
-        // </div>
+            <div className={styles.more} onClick={onResize}>
+                <p className={styles.moreText}>{size ? 'Скрыть' : 'Показать всё'}</p>
+                <div className={styles.moreIcon} style={size ? { transform: 'rotateZ(180deg)', marginTop: '-8px' } : { }}>{svgManager.getSvg('moreActivityContent')}</div>
+            </div>
+        </div>
     );
-
 }
 
 ProfileActivity.propTypes = {
