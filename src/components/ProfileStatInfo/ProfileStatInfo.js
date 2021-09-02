64f9458ed2bdf8999ce5578b8996a0d3562a71b1
pechenyice <german.iskhakov@mail.ru>
2021-09-02 23:08:46 +0300

    added Skeleton managment from deprecated manager

--- src/components/ProfileStatInfo/ProfileStatInfo.js
+++ src/components/ProfileStatInfo/ProfileStatInfo.js
@@ -8,19 +8,19 @@ import Skeleton from "../Skeleton/Skeleton";
 function ProfileStatInfo({content}) {
 
     return (
-        content.sessions.length && Object.keys(content.userInfo).length && Object.keys(content.updates).length ? 
+        !content.sessions.needFetch && Object.keys(content.userInfo).length && Object.keys(content.updates).length ? 
         <div className={styles.statInfoWrapperContainer}>
             <StatInfoSpec icon={svgManager.getSvg('statFriends')} fillSkeleton={false} hint={'друзья'} count={content.userInfo.friends}/>
             <StatInfoSpec icon={svgManager.getSvg('statSubs')} fillSkeleton={false} hint={'подписчики'} count={content.userInfo.subs}/>
             <StatInfoSpec icon={svgManager.getSvg('statLikes')} fillSkeleton={false} hint={'лайки под аватаркой'} count={content.userInfo.likes}/>
-            <StatInfoSpec icon={svgManager.getSvg('statSessions')} fillSkeleton={false} hint={'сессии за сегодня'} count={content.sessions.length}/>
+            <StatInfoSpec icon={svgManager.getSvg('statSessions')} fillSkeleton={false} hint={'сессии за сегодня'} count={content.sessions.entities.length}/>
             <StatInfoSpec icon={svgManager.getSvg('statChanges')} fillSkeleton={false} hint={'зафиксированные изменения'} count={Object.keys(content.updates).length ? content.updates.statuses.length + content.updates.avatars.length + content.updates.names.length : 0}/>
         </div> : 
         <div className={styles.statInfoWrapperContainer}>
             <StatInfoSpec icon={svgManager.getSvg('statFriends')} fillSkeleton={true} skeleton={<Skeleton type={'text'} animatorBackground={'var(--white)'} stylesheet={{width: '50%', marginTop: '10px'}} />} hint={'друзья'} count={content.userInfo.friends}/>
             <StatInfoSpec icon={svgManager.getSvg('statSubs')} fillSkeleton={true} skeleton={<Skeleton type={'text'} animatorBackground={'var(--white)'} stylesheet={{width: '50%', marginTop: '10px'}} />} hint={'подписчики'} count={content.userInfo.subs}/>
             <StatInfoSpec icon={svgManager.getSvg('statLikes')} fillSkeleton={true} skeleton={<Skeleton type={'text'} animatorBackground={'var(--white)'} stylesheet={{width: '50%', marginTop: '10px'}} />} hint={'лайки под аватаркой'} count={content.userInfo.likes}/>
-            <StatInfoSpec icon={svgManager.getSvg('statSessions')} fillSkeleton={true} skeleton={<Skeleton type={'text'} animatorBackground={'var(--white)'} stylesheet={{width: '50%', marginTop: '10px'}} />} hint={'сессии за сегодня'} count={content.sessions.length}/>
+            <StatInfoSpec icon={svgManager.getSvg('statSessions')} fillSkeleton={true} skeleton={<Skeleton type={'text'} animatorBackground={'var(--white)'} stylesheet={{width: '50%', marginTop: '10px'}} />} hint={'сессии за сегодня'} count={content.sessions.entities.length}/>
             <StatInfoSpec icon={svgManager.getSvg('statChanges')} fillSkeleton={true} skeleton={<Skeleton type={'text'} animatorBackground={'var(--white)'} stylesheet={{width: '50%', marginTop: '10px'}} />} hint={'зафиксированные изменения'} count={Object.keys(content.updates).length ? content.updates.statuses.length + content.updates.avatars.length + content.updates.names.length : 0}/>
         </div>
     );
