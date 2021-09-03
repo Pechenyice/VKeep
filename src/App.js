8fcbeaa44c09837bc03a7b6f29694ecfb92d3d99
pechenyice <german.iskhakov@mail.ru>
2021-09-03 23:01:00 +0300

    added router and handled user submit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import './App.css';
 import HomePage from './components/HomePage/HomePage';
 import ProfilePage from './components/ProfilePage/ProfilePage';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  Switch
+} from 'react-router-dom';
+import { useState } from 'react';
 
 function App() {
+
+  let [user, setUser] = useState('');
+
+  function handleUserSelected(vkUser) {
+    setUser(vkUser);
+    // location.pathname = [location.pathname, vkUser].join('/');
+  }
+
   return (
+    <Router>
+      <Switch>
+        <Route path={'/users'} render={() => <ProfilePage user={user} />} />
+        <Route path={'/'} render={() => <HomePage onUserSelected={handleUserSelected} />} />
+      </Switch>
+    </Router>
 
     // <HomePage /> 
 
-    <ProfilePage user={'bobby'}/>
+    // <ProfilePage user={'bobby'} />
 
   );
+
 }
 
 export default App;
