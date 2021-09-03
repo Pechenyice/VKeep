d9858028e5fe77a4cf1e9e9747c063d30cb69481
pechenyice <german.iskhakov@mail.ru>
2021-09-03 23:01:36 +0300

    added input validation

--- src/components/HomePage/HomePageForm.js
+++ src/components/HomePage/HomePageForm.js
@@ -1,37 +1,72 @@
 import React, { useState } from "react";
 import styles from './HomePage.module.css';
 import svgManager from "../../svgs/svgManager";
+import { Redirect, Link } from "react-router-dom";
 
-function HomePageForm() {
+function HomePageForm({ onUserSelected }) {
 
     let [vkUser, setVkUser] = useState('');
+    let [error, setError] = useState({
+        isActive: false,
+        content: ''
+    });
 
     function validateInput(input) {
-        console.log('I don\'t care your input, send me a porn video if u want')
+        if (!input) {
+            setError({ 'isActive': true, 'content': `Don't left this field empty please...` });
+            return false;
+        }
+
+        if (input.length < 3) {
+            setError({ 'isActive': true, 'content': `Value is too short` });
+            return false;
+        }
+
+        setError({ 'isActive': false, 'content': `` });
+        return true;
+    }
+
+    function handleInputIsNotInteresting() {
+        setError({ 'isActive': false, 'content': '' });
     }
 
     function handleVkUserInput(e) {
-
         validateInput(e.target.value);
 
         setVkUser(e.target.value);
     }
 
     function handleVkUserSubmit() {
-        setVkUser('');
+        if (validateInput(vkUser)) {
+            onUserSelected(vkUser);
+            setVkUser('');
+        }
     }
-    
+
+    let classes = [styles.input];
+
+    if (error.isActive) classes.push(styles.inputError);
+
     return (
         <div className={styles.formWrapper}>
             <label htmlFor='vkUser' className={styles.inputLabel}>Добавить пользователя</label>
             <div className={styles.formContainer}>
                 <div className={styles.inputWrapper}>
-                    <input className={styles.input} type='text' name='vkUser' placeholder='ID или domain' value={vkUser} onInput={handleVkUserInput}/>
-                    <div className={styles.svgWapper}>{svgManager.getSvg('userProfile')}</div>
+                    <input className={classes.join(' ')} type='text' name='vkUser' placeholder='ID или domain' value={vkUser} onBlur={handleInputIsNotInteresting} onInput={handleVkUserInput} />
+                    <div className={styles.svgWrapper}>{svgManager.getSvg('userProfile')}</div>
+                    <p className={styles.errorText}>{error.isActive ? error.content : ''}</p>
                 </div>
-                <button className={styles.inputSubmit} onClick={handleVkUserSubmit}>
-                    {svgManager.getSvg('buttonSubmit')}
-                </button>
+                {
+                    error.isActive ?
+                    <button className={styles.inputSubmit} onClick={handleVkUserSubmit}>
+                        {svgManager.getSvg('buttonSubmit')}
+                    </button> :
+                    <Link to={'/users'}>
+                        <button className={styles.inputSubmit} onClick={handleVkUserSubmit}>
+                            {svgManager.getSvg('buttonSubmit')}
+                        </button>
+                    </Link>
+                }
             </div>
         </div>
     );
