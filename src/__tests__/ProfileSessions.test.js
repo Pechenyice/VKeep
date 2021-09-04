41dc1a120e2f5f1f0624b9a2a8c7959a5eaa240a
pechenyice <german.iskhakov@mail.ru>
2021-09-04 15:50:20 +0300

    removed function rendered test

--- src/__tests__/ProfileSessions.test.js
+++ src/__tests__/ProfileSessions.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow, configure } from 'enzyme';
+import { shallow, configure, mount } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import ProfileSessions from '../components/ProfileSessions/ProfileSessions';
 import Skeleton from '../components/Skeleton/Skeleton';
@@ -13,48 +13,7 @@ describe('Shallow tests: Profile sessions', () => {
     const onHover = jest.fn();
     const onLeave = jest.fn();
 
-    describe('hover logic', () => {
-        beforeEach(() => {
-            wrapper = shallow(<ProfileSessions sessions={{'needFetch': false, 'entities': [
-                {
-                    'start': 1630000209000,
-                    'end': 1630000212000,
-                    'platform': 'PC'
-                },
-                {
-                    'start': 1630090214000,
-                    'end': 1630090216000,
-                    'platform': 'Android'
-                },
-                {
-                    'start': 1630100321000,
-                    'end': 1630100326000,
-                    'platform': 'Other'
-                }
-            ]}} onHover={onHover} onLeave={onLeave} />);
-        });
 
-        afterEach(() => {
-            onHover.mockClear();
-            onLeave.mockClear();
-        });
-    
-        it('should trigger hover mock', () => {
-            wrapper.find('.onlineSessionWrapper').first().simulate('mouseover');
-
-            expect(
-                onHover.mock.calls
-            ).toHaveLength(1);
-        });
-
-        it('should trigger leave mock', () => {
-            wrapper.find('.onlineSessionWrapper').first().simulate('mouseleave');
-            
-            expect(
-                onLeave.mock.calls
-            ).toHaveLength(1);
-        });
-    });
 
     describe('empty state', () => {
         beforeEach(() => {
