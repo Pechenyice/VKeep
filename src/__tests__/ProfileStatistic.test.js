a5e80e5ce8b3066e82a4175180fcda819d84a4d2
pechenyice <german.iskhakov@mail.ru>
2021-09-04 15:49:59 +0300

    fixed test

--- src/__tests__/ProfileStatistic.test.js
+++ src/__tests__/ProfileStatistic.test.js
@@ -11,9 +11,21 @@ configure({ adapter: new Adapter() });
 describe('Shallow tests: Profile statistic', () => {
     let wrapper;
 
+    describe('empty state', () => {
+        beforeEach(() => {
+            wrapper = shallow(<ProfileStatistic content={{'needFetch': true, 'entities': []}} platformSelected={''} />);
+        });
+    
+        it('should display skeletons', () => {
+            expect(
+                wrapper.find(Skeleton)
+            ).toHaveLength(1);
+        });
+    });
+
     describe('hover logic', () => {
         beforeEach(() => {
-            wrapper = shallow(<ProfileStatistic content={{}} platformSelected={'PC'} />);
+            wrapper = shallow(<ProfileStatistic content={{'needFetch': false, 'entities': []}} platformSelected={'PC'} />);
         });
 
         it('should trigger platform mock', () => {
@@ -22,17 +34,5 @@ describe('Shallow tests: Profile statistic', () => {
             ).toBe(true);
         });
     });
-
-    // describe('empty state', () => {
-    //     beforeEach(() => {
-    //         wrapper = shallow(<ProfileSessions sessions={{'needFetch': true, 'entities': []}} onHover={onHover} onLeave={onLeave} />);
-    //     });
-    
-    //     it('should display skeletons', () => {
-    //         expect(
-    //             wrapper.find(Skeleton)
-    //         ).toHaveLength(6);
-    //     });
-    // });
 
 });
