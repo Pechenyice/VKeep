e78ed22102ba9f2ea6069fe08920f0b29b774179
pechenyice <german.iskhakov@mail.ru>
2021-09-07 01:35:21 +0300

    refactor to react rules

--- src/components/SvgIconBlock/SvgIconBlock.js
+++ src/components/SvgIconBlock/SvgIconBlock.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styles from './SvgIconBlock.module.css';
 import PropTypes from 'prop-types';
-import svgManager from "../../svgs/svgManager";
 
 function SvgIconBlock({icon}) {
 
