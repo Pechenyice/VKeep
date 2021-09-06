4878b6bd91cc25f33c390c28ae7b7e7a72967607
pechenyice <german.iskhakov@mail.ru>
2021-09-07 01:33:59 +0300

    restyling

--- src/components/ProfileActivity/ProfileActivity.js
+++ src/components/ProfileActivity/ProfileActivity.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from './ProfileActivity.module.css';
 import PropTypes from 'prop-types';
 import ServiceName from "../ServiceName/ServiceName";
